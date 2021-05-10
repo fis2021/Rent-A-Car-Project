5907a9df80b599887508dde18467eac8b1e11cc3
irtudor <tudor.ir@yahoo.com>
2021-05-10 20:14:49 +0300

    RAC-7 #comment Test smart commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+        
         initDirectory();
         UserService.initDatabase();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
