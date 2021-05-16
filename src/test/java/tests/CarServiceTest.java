213f4fe9632d9eae77e77aeba85869ad386ef5f9
oanabadea98 <oana_2698@yahoo.com>
2021-05-16 16:14:42 +0300

    RAC-19 Tested GetMarci and GetLastId methods.

--- src/test/java/tests/CarServiceTest.java
+++ src/test/java/tests/CarServiceTest.java
@@ -40,4 +40,36 @@ class CarServiceTest
 
         Assert.assertSame(expectedOrase, foundOrase);
     }
+
+    @Test
+    public void testGetMarci()
+    {
+        Car car1 = new Car(1000, "marca1", 200000, "oras", 25, "5%", 9.5, 3, "Imageath", true, null, 0,  new LinkedList<String>());
+        Car car2 = new Car(1000, "marca2", 200000, "oras", 25, "5%", 9.5, 3, "Imageath", true, null, 0,  new LinkedList<String>());
+        Car car3 = new Car(1000, "marca3", 200000, "oras", 25, "5%", 9.5, 3, "Imageath", true, null, 0,  new LinkedList<String>());
+        CarService.addCar(car1.getMarca(), car1.getKilometri(), car1.getOras(), car1.getPret(), car1.getConsum(), car1.getImagePath());
+        CarService.addCar(car2.getMarca(), car2.getKilometri(), car2.getOras(), car2.getPret(), car2.getConsum(), car2.getImagePath());
+        CarService.addCar(car3.getMarca(), car3.getKilometri(), car3.getOras(), car3.getPret(), car3.getConsum(), car3.getImagePath());
+
+        LinkedList<String> foundMarci = CarService.getMarci();
+        LinkedList<String> expectedMarci = new LinkedList<String>();
+        expectedMarci.add("marca1");
+        expectedMarci.add("marca2");
+        expectedMarci.add("marca3");
+
+        Assert.assertSame(expectedMarci, foundMarci);
+    }
+
+    @Test
+    public void testGetLastId()
+    {
+        int oldLastId = CarService.getLastId();
+
+        Car car = new Car(1000, "marca", 200000, "oras", 25, "5%", 9.5, 3, "Imageath", true, null, 0,  new LinkedList<String>());
+        CarService.addCar(car.getMarca(), car.getKilometri(), car.getOras(), car.getPret(), car.getConsum(), car.getImagePath());
+
+        int newLastId = CarService.getLastId();
+
+        Assert.assertEquals(oldLastId+1, newLastId);
+    }
 }
